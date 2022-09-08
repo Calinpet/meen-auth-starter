2a5ca3dd6a90162281886045cf1f81b72520c84d
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-09-08 14:28:56 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+
+// Dependencies
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const mongoose = require('mongoose');
+const session = require('express-session');
+
+// Database Configuration
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+// Database Connection Error / Success
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+
+// Middleware
+// Body parser middleware: give us access to req.body
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+      secret: process.env.SECRET,
+      resave: false,
+      saveUninitialized: false
+  }));
+
+// Routes / Controllers
+const userController = require('./controllers/users');
+app.use('/users', userController);
+
+const sessionsController = require('./controllers/sessions');
+app.use('/sessions', sessionsController);
+
+
+
+//INDUCES 
+
+//INDEX
+//NEW
+//DELETE
+//UPDATE
+//CREATE
+//EDIT
+//SHOW
+
+
+
+// Listener
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
